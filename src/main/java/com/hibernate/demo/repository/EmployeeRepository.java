320b76faa480c45bd9e4c4c34fa224e953c17131
Li Zhang <1057730037@qq.com>
2020-01-06 17:23:12 +0800

    [N/A][lizhang] add h2 database

---
+++ src/main/java/com/hibernate/demo/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.hibernate.demo.repository;
+
+import com.hibernate.demo.model.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository
+        extends JpaRepository<EmployeeEntity, Long> {
+ 
+}
